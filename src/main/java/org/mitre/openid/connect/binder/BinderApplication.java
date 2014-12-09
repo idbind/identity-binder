d69562491d90d2b8afe0a9e41befe8a583258d0b
William Kim <wikkim@gmail.com>
2014-12-09 15:51:55 -0500

    changed from deployable jar to war build.

--- src/main/java/org/mitre/openid/connect/binder/BinderApplication.java
+++ src/main/java/org/mitre/openid/connect/binder/BinderApplication.java
@@ -2,13 +2,20 @@ package org.mitre.openid.connect.binder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class BinderApplication {
+public class BinderApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BinderApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BinderApplication.class);
     }
 }
--- src/main/java/org/mitre/openid/connect/binder/config/WebConfiguration.java
+++ src/main/java/org/mitre/openid/connect/binder/config/WebConfiguration.java
@@ -1,10 +1,12 @@
 package org.mitre.openid.connect.binder.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Override
