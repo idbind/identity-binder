7800d5eb13cb08dd1535db2ee2bf7e30ef19dcda
William Kim <wikkim@gmail.com>
2015-01-21 16:26:23 -0500

    added per user query

--- src/main/java/org/mitre/openid/connect/binder/web/IdentityQueryController.java
+++ src/main/java/org/mitre/openid/connect/binder/web/IdentityQueryController.java
@@ -2,12 +2,14 @@ package org.mitre.openid.connect.binder.web;
 
 import java.util.Set;
 
+import org.mitre.openid.connect.binder.model.MultipleIdentity;
 import org.mitre.openid.connect.binder.model.SingleIdentity;
 import org.mitre.openid.connect.binder.service.IdentityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +27,11 @@ public class IdentityQueryController {
 		return identityService.getAllIdentities();
 	}
 	
-	
+	@PreAuthorize("#oauth2.hasScope('org.mitre.idbind.query')")
+	@RequestMapping(value = "/query", method = RequestMethod.POST, consumes = "application/x-www-form-urlencoded", produces = "application/json")
+	public Set<SingleIdentity> queryUser(@RequestParam("issuer") String issuer, @RequestParam("subject") String subject) {
+		MultipleIdentity identity = identityService.getMultipleBySubjectIssuer(subject, issuer); 
+		return identity == null ? null : identity.getIdentities();
+	}
 	
 }
