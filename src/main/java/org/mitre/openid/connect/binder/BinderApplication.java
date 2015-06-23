0c57f38be25a54e0be49a48bd1251dc516584a46
William Kim <wikkim@gmail.com>
2015-06-23 13:25:11 -0400

    moved Properties bean to top config level, and added comment for boolean zen

--- src/main/java/org/mitre/openid/connect/binder/BinderApplication.java
+++ src/main/java/org/mitre/openid/connect/binder/BinderApplication.java
@@ -1,9 +1,12 @@
 package org.mitre.openid.connect.binder;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @ComponentScan
@@ -12,6 +15,15 @@ public class BinderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BinderApplication.class, args);
+    }
+    
+    @Bean
+    public static PropertyPlaceholderConfigurer properties() {
+    	PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+    	ClassPathResource[] resources = new ClassPathResource[] { new ClassPathResource("websecurity.properties") };
+    	ppc.setLocations(resources);
+    	ppc.setIgnoreUnresolvablePlaceholders(true);
+    	return ppc;
     }
    
 }
